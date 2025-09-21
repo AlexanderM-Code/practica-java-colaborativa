66c6c57e323f3928e67fb4e220433404314a2a1b
santiago <santiago.rodriguezj@upb.edu.co>
2025-09-20 20:20:58 -0500

    Agregado método calcularPromedio en Estudiante

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -12,4 +12,18 @@ public class Estudiante {
         this.notas = new double[capacidadNotas];
         this.cantidadNotas = 0;
     }
+
+
+    // Método: Calcular Promedio
+    public double calcularPromedio() {
+        if (cantidadNotas == 0) {
+            return 0; // Evitar división por cero si no hay notas
+        }
+
+        double suma = 0;
+        for (int i = 0; i < cantidadNotas; i++) {
+            suma += notas[i];
+        }
+        return suma / cantidadNotas;
+    }
 }
